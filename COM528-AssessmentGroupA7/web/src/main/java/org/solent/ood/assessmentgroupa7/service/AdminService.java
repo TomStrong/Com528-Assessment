c1e100c9783953d59dde2e388d610c4b0c141871
jorandall <5randj17@solent.ac.uk>
2021-11-02 15:00:04 +0000

    Updated AdminService class

--- COM528-AssessmentGroupA7/web/src/main/java/org/solent/ood/assessmentgroupa7/service/AdminService.java
+++ COM528-AssessmentGroupA7/web/src/main/java/org/solent/ood/assessmentgroupa7/service/AdminService.java
@@ -5,10 +5,33 @@
  */
 package org.solent.ood.assessmentgroupa7.service;
 
+
+import org.solent.ood.assessmentgroupa7.dao.PropertiesDao;
+import org.solent.ood.assessmentgroupa7.dao.WebObjectFactory;
+
 /**
  *
- * @author jofai
+ * @author jrandall
  */
+
 public class AdminService {
+    
+    private PropertiesDao propertiesDao = WebObjectFactory.getPropertiesDao();
+    
+    public void getUser (){
+        String name = propertiesDao.getProperty("org.solent.ood.assessmentgroupa7.name");
+        String endDate = propertiesDao.getProperty("org.solent.ood.assessmentgroupa7.enddate");
+        String cardNumber = propertiesDao.getProperty("org.solent.ood.assessmentgroupa7.cardno");
+        String cvv = propertiesDao.getProperty("org.solent.ood.assessmentgroupa7.cvv");
+        String issueNumber = propertiesDao.getProperty("org.solent.ood.assessmentgroupa7.issueno");
+    };
+    
+    public void setUser(String name, String endDate, String cardNumber, String cvv, String issueNumber){
+        propertiesDao.setProperty("org.solent.ood.assessmentgroupa7.name", name);
+        propertiesDao.setProperty("org.solent.ood.assessmentgroupa7.enddate", endDate);
+        propertiesDao.setProperty("org.solent.ood.assessmentgroupa7.cardno", cardNumber);
+        propertiesDao.setProperty("org.solent.ood.assessmentgroupa7.cvv", cvv);
+        propertiesDao.setProperty("org.solent.ood.assessmentgroupa7.issueno", issueNumber);
+    };
     
 }
